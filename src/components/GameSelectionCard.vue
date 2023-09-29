b694ef07851a11161610abdd589703269da7b70b
Michael Stokoe <mstokoe0990@gmail.com>
2023-09-29 18:24:01 +0100

    Home page built

---
+++ src/components/GameSelectionCard.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="flex flex-col space-y-4">
+        <router-link :to="{
+            name: 'game',
+            params: {
+                opponent: gameName,
+            }
+        }" :class="{
+    'pointer-events-none': disabled,
+}">
+            <div class="rounded-xl" :class="{
+                'transition-all cursor-pointer hover:shadow-xl hover:scale-105': !disabled,
+            }">
+                <div class="flex">
+                    <div class="relative">
+                        <img :src="image" class="rounded-xl">
+                        <div v-if="disabled"
+                            class="absolute top-0 left-0 z-50 w-full h-full bg-black rounded-xl opacity-90">
+                            <div class="flex flex-col justify-center h-full text-center">
+                                <span class="text-3xl">Coming soon!</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </router-link>
+        <div class="flex flex-col space-y-2 text-center">
+            <h1 class="text-4xl font-semibold">{{ gameTitle }}</h1>
+
+            <p>{{ gameDescription }}</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref, computed, watch, onMounted } from "vue";
+import { useRoute, useRouter } from 'vue-router';
+
+const props = defineProps({
+    image: {
+        type: String,
+        required: true,
+    },
+    disabled: {
+        type: Boolean,
+        default: false,
+        required: false,
+    },
+    gameTitle: {
+        type: String,
+        required: true,
+    },
+    gameDescription: {
+        type: String,
+        required: true,
+    },
+    gameName: {
+        type: String,
+        required: true,
+    },
+});
+</script>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,7 +1,25 @@
 <template>
-    Home page
+    <div class="container mx-auto">
+        <div class="grid grid-cols-3 gap-6">
+            <game-selection-card :gameTitle="'Battle the Horde'" :image="'/images/minotaur.png'"
+                :gameDescription="'Face of against the relentless Minotaur Horde'" :gameName="'minotaur'" />
+            <game-selection-card :gameTitle="'Face the Hydra'" :image="'/images/hydra.png'" :disabled="true"
+                :gameName="'hydra'" :gameDescription="'Cut off a head, another grows back.'" />
+            <game-selection-card :gameTitle="'Defeat a God'" :image="'/images/god.png'" :disabled="true" :gameName="'god'"
+                :gameDescription="'Xenagos and his revelers are coming for you.'" />
+        </div>
+    </div>
 </template>
 
 <script setup>
 import { ref, computed, watch, onMounted } from "vue";
+import { useRoute, useRouter } from 'vue-router';
+
+const route = useRoute();
+const router = useRouter();
+
+import GameSelectionCard from '../components/GameSelectionCard.vue';
+const components = {
+    GameSelectionCard
+}
 </script>
