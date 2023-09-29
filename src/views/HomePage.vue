d880f53788936dc83df0dd3e122d94d31a6fe5c8
Michael Stokoe <mstokoe0990@gmail.com>
2023-09-29 18:28:23 +0100

    HomePage stuff

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,5 +1,20 @@
 <template>
-    <div class="container mx-auto">
+    <div class="container flex flex-col mx-auto space-y-6 text-center">
+        <div>
+            <p>
+                <span class="font-beleren">Goldfishr</span> is the easiest way to test your decks' mettle. It will handle
+                its own turns, casting
+                spells and moving to combat, declare attackers, etc.
+            </p>
+            <p>We have a built-in life counter which will automatically deduct life lost from unblocked attackers and any
+                spells
+                cast by the opponent.</p>
+            <p>Currently, only the original Theros block's "Battle the Horde" deck is ready, but we aim to have the other
+                two
+                done soon!</p>
+            <p class="text-lg font-semibold">Get started by choosing your opponent below!</p>
+        </div>
+
         <div class="grid grid-cols-3 gap-6">
             <game-selection-card :gameTitle="'Battle the Horde'" :image="'/images/minotaur.png'"
                 :gameDescription="'Face of against the relentless Minotaur Horde'" :gameName="'minotaur'" />
