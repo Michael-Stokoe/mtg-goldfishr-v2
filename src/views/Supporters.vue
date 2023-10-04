e92a78d5123f584b73dd750a20b961ba7e3a852a
Michael Stokoe <mstokoe0990@gmail.com>
2023-10-04 15:31:18 +0100

    Supporters styling

--- src/views/Supporters.vue
+++ src/views/Supporters.vue
@@ -12,12 +12,16 @@
 
     <div class="flex justify-center text-center">
         <div class="flex flex-col w-full max-w-5xl space-y-6">
-            <div v-for="supporter in supporters" class="flex flex-col w-full p-6 space-y-3 text-left bg-black border-2 bg-opacity-80 border-neutral-300 rounded-xl">
+            <div v-for="supporter in supporters" class="flex flex-col w-full p-6 space-y-3 text-left bg-black border-2 shadow-xl bg-opacity-80 border-neutral-300 rounded-xl">
                 <h2 class="text-3xl font-semibold text-white">{{ supporter.name }}</h2>
                 <p v-html="supporter.description" class="text-gray-500"></p>
                 <p>Check him out here: <a :href="supporter.url" target="_blank" class="text-xl text-gray-300 hover:underline hover:text-white"><i class="ml-2 text-base fa-solid fa-link"></i> Socials</a></p>
             </div>
         </div>
+    </div>
+
+    <div class="flex max-w-5xl mx-auto mt-8">
+        <a href="https://linktr.ee/stokoe0990" class="text-neutral-300 hover:text-white hover:underline">Get your name here by becoming a supporter</a>
     </div>
 </template>
 
