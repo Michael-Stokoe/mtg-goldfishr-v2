a82ae05261be1470fc6c8b7d9d934c591c94580b
Michael Stokoe <mstokoe0990@gmail.com>
2023-10-05 12:41:30 +0100

    safelist classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,14 +10,12 @@ export default {
   plugins: [],
   safelist: [
     {
-      pattern: /bg-(green|red|blue|black|white|neutral)-(100|200|300|400|500|600|700|800|900)/
+      pattern: /bg-(green|red|blue|black|white|neutral)-(100|200|300|400|500|600|700|800|900)/,
+      variants: ['hover', 'focus']
     },
     {
       pattern: /outline-(green|red|blue|black|white|neutral)-(100|200|300|400|500|600|700|800|900)/
     },
-    {
-      pattern: /focus-visible:outline-(green|red|blue|black|white|neutral)-(100|200|300|400|500|600|700|800|900)/
-    }
   ],
 }
 
